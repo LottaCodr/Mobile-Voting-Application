676cc81af44dd5dc8f22e0d020719fbd324fda29
King-Lota <lottanna47@gmail.com>
2024-03-13 11:32:58 +0100

    Party Model added

---
+++ lib/models/party_model.dart
@@ -0,0 +1,12 @@
+class Party {
+  final String id;
+  final String name;
+  final String symbol;
+  final String manifesto;
+
+  Party(
+      {required this.id,
+      required this.name,
+      required this.symbol,
+      required this.manifesto});
+}
