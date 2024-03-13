de2061a03ff33a250450286f2d04b2690fc4e799
King-Lota <lottanna47@gmail.com>
2024-03-13 12:00:01 +0100

    Party Model added

--- lib/models/candidate_model.dart
+++ lib/models/candidate_model.dart
@@ -1,8 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Candidate {
   final String id;
   final String name;
-  final String party;
+  final String partyId;
   final String manifesto;
 
-  Candidate({required this.id, required this.name, required this.party, required this.manifesto});
+  Candidate(
+      {required this.id,
+      required this.name,
+      required this.partyId,
+      required this.manifesto});
+
+  factory Candidate.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Candidate(
+      id: doc.id,
+      name: data['name'],
+      partyId: data['partyId'],
+      manifesto: data[''],
+    );
+  }
 }
