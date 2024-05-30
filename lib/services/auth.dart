bb859a0ac21042449c77c49d12cba0169fdbc1b7
King-Lota <lottanna47@gmail.com>
2024-05-30 02:32:09 +0100

    Firebase Auth Definitions

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,10 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-// class AuthService {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  User? get currentUser => _auth.currentUser;
 
 //   //sign in anon
 
@@ -17,31 +20,38 @@
 //   }
 // }
 
-//import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+  Future signIn({required String email, required String password}) async {
+    if (passwordConfirmed()) {
+      await _auth.signInWithEmailAndPassword(
+          email: 'email'.trim(), password: 'password'.trim());
 
-Future signIn() async {
-  if (passwordConfirmed()) {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: 'email'.trim(), password: 'password'.trim());
-
-    //add user details
+      //add user details
+    }
   }
-}
 
-bool passwordConfirmed() {
-  if ('password'.trim() == 'confirme password'.trim()) {
-    return true;
-  } else {
-    return false;
+  bool passwordConfirmed() {
+    if ('password'.trim() == 'confirmed password'.trim()) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
 
+  Future createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future signOut() async {
+    await _auth.signOut();
+  }
 // Future addUserDetails(String firstName, String lastName, String email, String password) async {
 //   await FirebaseFirestore.instance.collection('users').add(
-//     'first name' : firstName,
-//     'last name'; lastName,
+//      firstName,
+//      lastName,
 //     'email': email,
 //     'password': password
 //   );
 // }
+}
